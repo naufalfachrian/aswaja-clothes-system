9df0543b82beb48814c24844aee1abb284637aa3
Naufal Fachrian <naufal.fachrian@gmail.com>
2018-11-20 11:15:56 +0700

    fix connection secret and database

---
+++ src/aswajaclothes/connection/ConnectionSecret.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aswajaclothes.connection;
+
+/**
+ *
+ * @author littleflower
+ */
+public class ConnectionSecret {
+    
+    static String databaseName = "aswaja_clothes";
+    
+    static String databaseUsername = "";
+    
+    static String databasePassword = "";
+    
+}
